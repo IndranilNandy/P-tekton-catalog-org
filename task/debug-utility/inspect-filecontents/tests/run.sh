de58ba20b362fd52b72ff2153a50fd4c19a7ecab
IndranilNandy <indranilnandy@devbox6.0>
2024-08-06 20:14:55 +0530

    Updated

---
+++ task/debug-utility/inspect-filecontents/tests/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+(
+    cd ../../../../scripts/tests
+    ./init_test.sh
+)
+
+kubectl create -f run.yaml
+
+echo -e "Run tear_test.sh once the runs are completed, to restore your environment (configmaps etc)"
