b0eba44d4785059067fb9806b0c7d5f44b49acac
IndranilNandy <indranilnandy@devbox6.0>
2024-08-11 20:51:56 +0530

    [bot][devbox6][indranilnandy][Sun Aug 11 08:51:56 PM IST 2024] Remote updated with latest

--- pipeline/build-code-and-image/tests/run.sh
+++ pipeline/build-code-and-image/tests/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 (
-    cd ../../../../scripts/tests
+    cd ../../../scripts/tests
     ./init_test.sh
 )
 
 kubectl apply -f resources.yaml
 
 (
-    cd ../../../../scripts/tests/secrets/yaml-generator
+    cd ../../../scripts/tests/secrets/yaml-generator
 
     echo -e
     echo -e "Creating configs/secrets"
@@ -17,8 +17,6 @@ kubectl apply -f resources.yaml
 )
 
 kubectl apply -f serviceaccount.yaml
-
-kubectl apply -f pipeline.yaml
 kubectl create -f run.yaml
 
 echo -e "Run tear_test.sh once the runs are completed, to restore your environment (configmaps etc)"
