55194f962f786374203e8fdfcd3625aab275f96b
IndranilNandy <indranilnandy@devbox6.0>
2024-08-14 13:47:58 +0530

    [bot][devbox6][indranilnandy][Wed Aug 14 01:47:58 PM IST 2024] Remote updated with latest

--- pipeline/build-code-and-image/samples/docker-context-not-mounted/tear_test.sh
+++ pipeline/build-code-and-image/samples/docker-context-not-mounted/tear_test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-kubectl delete -f pipeline.yaml
+# kubectl delete -f pipeline.yaml
 kubectl delete -f serviceaccount.yaml
 kubectl delete secret test-git-ssh-secret
 kubectl delete secret test-docker-credentials
 kubectl delete -f resources.yaml
 
 (
-    cd ../../../../../scripts/tests
+    cd ../../../../scripts/tests
     ./tear_test.sh
 )
