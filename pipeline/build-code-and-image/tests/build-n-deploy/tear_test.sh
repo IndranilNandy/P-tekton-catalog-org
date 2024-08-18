c4ed25c102b634e2a613257dc9cd7c8f31d3bca8
IndranilNandy <indranilnandy@devbox6.0>
2024-08-18 12:06:32 +0530

    [bot][devbox6][indranilnandy][Sun Aug 18 12:06:32 PM IST 2024] Remote updated with latest

--- pipeline/build-code-and-image/tests/build-n-deploy/tear_test.sh
+++ pipeline/build-code-and-image/tests/build-n-deploy/tear_test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # kubectl delete -f pipeline.yaml
+kubectl delete -f ../../build-n-deploy-pipeline.yaml
+kubectl delete -f ../../deploy-pipeline.yaml
+kubectl delete -f ../../build-pipeline.yaml
 kubectl delete -f serviceaccount.yaml
 kubectl delete secret test-git-ssh-secret
-# kubectl delete secret test-docker-credentials
+kubectl delete secret test-docker-credentials
 kubectl delete -f resources.yaml
 
 (
