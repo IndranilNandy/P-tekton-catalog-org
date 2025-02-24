599f6c1b108ec7870c85ab853e8e322f8f251a7b
IndranilNandy <indranilnandy@devbox8>
2025-02-25 00:00:08 +0530

    [bot][devbox8][indranilnandy][Tue Feb 25 12:00:08 AM IST 2025] Remote updated with latest

---
+++ pip/tests/branch-protections/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+(
+    cd ../../../../scripts/tests/secrets/yaml-generator
+
+    echo -e
+    echo -e "Creating configs/secrets"
+    echo "echo -e \"$(cat git-ssh-auth-generic.sh.yaml)\"" | bash | kubectl apply -f -
+    # echo "echo -e \"$(cat docker-credentials-generic.sh.yaml)\"" | bash | kubectl apply -f -
+)
+
+kubectl apply -f serviceaccount.yaml
+kubectl create -f run.yaml
+
+echo -e "Run tear_test.sh once the runs are completed, to restore your environment (configmaps etc)"
---
+++ pip/tests/branch-protections/tear_test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+kubectl delete -f serviceaccount.yaml
+
+(
+    cd ../../../../scripts/tests
+    ./tear_test.sh
+)
